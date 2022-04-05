20f85d3b5567a77129e36c2ea4514d688ddb4e64
kyosuke <kyosuke.ito@gmail.com>
2022-04-04 18:37:23 -0700

    connect to mongoose

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
@@ -8,3 +9,14 @@ const app = express();
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend server is runnnig port number ${process.env.PORT}`);
 });
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("DB connection successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+dbConnect();
